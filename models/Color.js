282ed7d5cd4e217a2f131e84c10687c915d5b810
okFox <smayfield@gmail.com>
2019-12-05 14:38:13 -0800

    installs

---
+++ models/Color.js
@@ -0,0 +1,29 @@
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  red: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255
+  },
+  green: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255
+  },
+  blue: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255
+  }
+});
+
+module.exports = mongoose.model('Color', schema);
